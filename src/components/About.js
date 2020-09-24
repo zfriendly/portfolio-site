da1c57569a4076bf54096380dc8275d607d9b9b4
Zack Friendly <friendly.wisconsin@gmail.com>
2020-09-24 16:29:52 -0400

    edited about and skills

--- src/components/About.js
+++ src/components/About.js
@@ -16,25 +16,29 @@ const About = () => {
         <Header size="huge">// About Me</Header>
         <Header className="about_textboxes">
           I am a resourceful software engineer with a knack for ideating
-          creative solutions to complex problems. Experienced in working across
-          a wide range of disciplines, from app-building and software debugging
-          to large-scale event production to discreet investigations. Compelled
-          by a desire to understand the entirety of an issue and a willingness
-          to dig into the minutiae.
+          creative solutions to complex problems. My career as a private
+          investigator by day and music events producer by night allowed me
+          develop a capacity to independently problem-solve and balance multiple
+          competing deadlines and tight budgets. I approach each problem with
+          thoughftulness and a willingness to work within a team to reach a
+          shared goal. I am experienced in working across a wide range of
+          disciplines, from app-building and software debugging to large-scale
+          event production to discreet investigations.
         </Header>
         <Header size="huge">// Skills</Header>
         <Header className="about_textboxes">
-          <span className="skill_titles">Front-End: </span> HTML, CSS,
-          Javascript, React, Vue, Gatsby/GraphQL
+          <span className="skill_titles">Programming Languages: </span>{" "}
+          JavaScript, Python, SQL, CSS, HTML
           <br />
-          <span className="skill_titles">Back-End: </span> Node, Express,
-          Python, Django, MongoDB, PostgreSQL
+          <span className="skill_titles">Platforms and Frameworks: </span>{" "}
+          React, Node, Vue, Gatsby, Django, Express, MongoDB, GIT
           <br />
-          <span className="skill_titles">Deployment/Management: </span> Git,
-          Github, NPM, Heroku, Pip
+          <span className="skill_titles">Other skills: </span> RESTful APIs,
+          GitHub, Mongoose, Agile, Heroku, Bootstrap, MaterialUI, Storybook,
+          Netlify, Peewee, Application and Database Object-Oriented Design,
+          Encapsulation, Testing, Data Structures, Debugging, Responsive Design,
+          Adobe Photoshop, Salesforce, CRM
           <br />
-          <span className="skill_titles">Other Skills: </span> Adobe
-          Photoshop/XD, Jira, Trello, Salesforce
         </Header>
         <div className="about_icons">
           <a href="https://github.com/zfriendly" target="_blank">
